8cc44ec1bdc8b88b6ac17e4c0d0419ef5894e428
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2024-08-21 15:44:53 +0900

    (2024.8.21) 충돌 시스템

---
+++ Game/Assets/Class9th (Collision)/Scripts/Knight.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum State
+{
+    WALK,
+    ATTACK,
+    DIE
+}
+
+public class Knight : MonoBehaviour
+{
+    [SerializeField] State state;
+    [SerializeField] Animator animator;
+
+    [SerializeField] float speed = 2.5f;
+
+    void Start()
+    {
+        state = State.WALK;
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        switch (state) 
+        {
+          case State.WALK : Walk();
+            break;
+          case State.ATTACK : Attack(); 
+            break;
+          case State.DIE : Die(); 
+            break;
+        }
+    }
+
+    public void Walk()
+    {
+        animator.SetBool("Attack", false);
+
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    public void Attack()
+    {
+        animator.SetBool("Attack", true);
+    }
+
+    public void Die()
+    {
+        animator.Play("Die");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            state = State.ATTACK;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("OnTriggerStay");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            state = State.DIE;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // OnCollisionEnter : 물리적인 충돌이 일어났을 때 호출되는 이벤트 함수입니다.
+        Debug.Log("OnCollisionEnter");
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        // OnCollisionStay : 물리적인 충돌을 하고 있는 중일 때 호출되는 이벤트 함수입니다.
+        Debug.Log("OnCollisionStay");
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        // OnCollisionExit : 물리적인 충돌이 끝났을 때 호출되는 이벤트 함수입니다.
+        Debug.Log("OnCollisionExit");
+    }
+}
