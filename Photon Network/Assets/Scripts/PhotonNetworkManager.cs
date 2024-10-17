c1aaaf25686e4f7d4ad0f0eb9d247ff09c9a570c
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2024-10-17 15:41:40 +0900

    (2024.10.17) 로비 접속 시스템

--- Photon Network/Assets/Scripts/PhotonNetworkManager.cs
+++ Photon Network/Assets/Scripts/PhotonNetworkManager.cs
@@ -25,7 +25,7 @@ public class PhotonNetworkManager : MonoBehaviourPunCallbacks
 
     public void Failure(PlayFabError playFabError)
     {
-        Debug.Log(playFabError.GenerateErrorReport());
+        PopUpManager.Instance.Show(AlarmType.SIGNINFAILURE, playFabError.GenerateErrorReport());
     }
 
     public void OnSignUp()
