f64939e5949e08998e99e5dbadde8b7d9b3c7f35
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-04-11 15:41:10 +0900

    (2025.4.11) 로비 접속 시스템

---
+++ Fall Guys/Assets/Scripts/Information.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class Information : MonoBehaviourPunCallbacks
+{
+    [SerializeField] string roomName;
+
+    [SerializeField] Text description;
+
+    public void OnConnectRoom()
+    {
+        PhotonNetwork.JoinRoom(roomName);
+    }
+
+    public void View(string name, int currentPersonnel, int maxPersonnel)
+    {
+        roomName = name;
+
+        description.text = roomName + " ( " + currentPersonnel + " / " + maxPersonnel + " )";
+    }
+}
