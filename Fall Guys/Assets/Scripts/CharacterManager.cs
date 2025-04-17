5e42532cad454e11ca6f0801a153cf54b38af017
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-04-17 15:43:59 +0900

    (2025.4.17) 동기화 시스템

--- Fall Guys/Assets/Scripts/CharacterManager.cs
+++ Fall Guys/Assets/Scripts/CharacterManager.cs
@@ -5,11 +5,27 @@ using UnityEngine;
 
 public class CharacterManager : MonoBehaviour
 {
+    [SerializeField] List<Transform> transformList = new List<Transform>();
+
+    private void Awake()
+    {
+        CreateTransform();
+    }
+
     void Start()
     {
-        Debug.Log(PhotonNetwork.CurrentRoom.PlayerCount);
+        int index = PhotonNetwork.CurrentRoom.PlayerCount - 1;
 
+        PhotonNetwork.Instantiate("Character", transformList[index].position, Quaternion.identity);
+    }
 
-        PhotonNetwork.Instantiate("Character", Vector3.zero, Quaternion.identity);
+    void CreateTransform()
+    {
+        for(int i = 0; i < PhotonNetwork.CurrentRoom.MaxPlayers; i++)
+        {
+            Transform clone = Instantiate(Resources.Load<Transform>("Create Position " + i));
+
+            transformList.Add(clone);
+        }
     }
 }
