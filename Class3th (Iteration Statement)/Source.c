b6a242484c0affe346e081d5653a368366add1c2
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-05-23 15:42:57 +0900

    (2025.5.23) 반복문

---
+++ Class3th (Iteration Statement)/Source.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+
+void main()
+{
+#pragma region 반복문
+	// 프로그램 내에서 특정한 작업을 반복적으로 
+	// 수행하는 명령문입니다.
+
+#pragma region 증감 연산자
+	// 피연산자를 하나씩 증가시키거나 감소시킬 때
+	// 사용하는 연산자입니다.
+
+#pragma region 전위 증감 연산자
+	// 변수의 값을 증감시킨 후에 연산을 수행하는 연산자입니다.
+
+	//	int protine = 0;
+	//	
+	//	int calorie = ++protine;
+	//	
+	//	printf("protine 변수의 값 : %d\n", protine);
+	//	printf("calorie 변수의 값 : %d\n", calorie);
+	//	
+	//	calorie = --protine;
+	//	
+	//	printf("protine 변수의 값 : %d\n", protine);
+	//	printf("calorie 변수의 값 : %d\n", calorie);
+
+#pragma endregion
+
+#pragma region 후위 증감 연산자
+	// 연산을 수행한 다음 변수의 값을 증감시키는 연산자입니다.
+
+	// int x = 0;
+	// int position = x++;
+	// 
+	// printf("position : %d\n", position);
+	// printf("x: %d\n", x);
+	// 
+	// position = x--;
+	// 
+	// printf("position : %d\n", position);
+	// printf("x : %d\n", x);
+
+#pragma endregion
+
+#pragma endregion
+
+#pragma region for문
+	// 초기식을 연산하여 조건식의 결과에 따라 특정한
+	// 수만큼 반복하는 반복문입니다.
+
+	//	for (int i = 0; i < 5; i++)
+	//	{
+	//		printf("Execute...\n");
+	//	}
+
+	// 반복문이 동작하는 순서는 "초기화 -> 조건 검사 -> 실행
+	// -> 증감의 구조"로 동작합니다.
+#pragma endregion
+
+#pragma region while문
+	// 특정 조건을 만족할 때까지 계속해서 주어진
+	// 명령문을 실행하는 반복문입니다.
+
+	//	int count = 0;
+	//	
+	//	while (count < 5)
+	//	{
+	//		printf("count : %d\n", count);
+	//	
+	//		count++;
+	//	}
+
+	// 반복문을 순차적으로 실행하면서 조건 분기를 만나게 되면
+	// 어느 쪽으로 실행 흐름이 갈 지 미리 예측입니다.
+#pragma endregion
+
+#pragma region do while문
+	// 조건과 상관없이 한 번의 작업을 수행한 다음
+	// 조건에 따라 명령문을 실행하는 반복문입니다.
+
+	//	int connect = 0;
+	//	
+	//	do
+	//	{
+	//		printf("Access");
+	//	} while (connect > 3);
+
+	// 반복문의 경우 미리 예측해서 실행하는 구조를 가지며,
+	// 예측이 틀렸다면 현재 반복문 내의 조건 분기만 다시
+	// 검사하여 처리합니다.
+#pragma endregion
+
+#pragma region nested for문
+
+	//	for (int i = 0; i < 3; i++)
+	//	{
+	//		printf("i의 값 : %d\n\n", i);
+	//	
+	//		for (int j = 0; j < 3; j++)
+	//		{
+	//			printf("j의 값 : %d\n", j);
+	//		}
+	//	
+	//		printf("\n");
+	//	}
+
+#pragma endregion
+
+#pragma endregion
+}
--- Program/Source.c
+++ Program/Source.c
@@ -2,7 +2,80 @@
 
 void main()
 {
+#pragma region 조건문
+	// 어떤 조건이 주어질 때 해당 조건에 따라 동작을
+	// 수행하도록 실행하는 명령문입니다.
 
+#pragma region 관계 연산자
+	// 두 개의 피연산자의 값을 비교하여 그 결과를 0 또는
+	// 1이라는 값으로 나타내는 연산자입니다.
+
+	// >  : 왼쪽의 값이 더 크다면
+	// <  : 오른쪽의 값이 더 크다면
+
+	// >= :	왼쪽의 값이 크거나 같다면
+	// <= : 오른쪽의 값이 크거나 같다면
+
+	// == : 값이 서로 같다면
+	// != : 값이 서로 다르다면
+
+	// int storage1 = 10 > 5;
+	// int storage2 = 10 < 5;
+	// int storage3 = 10 >= 5;
+	// int storage4 = 10 <= 5;
+	// int storage5 = 10 == 1;
+	// int storage6 = 10 != 1;
+	// 
+	// printf("storage1 : %d\n", storage1);
+	// 
+	// printf("storage2 : %d\n", storage2);
+	// 
+	// printf("storage3 : %d\n", storage3);
+	// 
+	// printf("storage4 : %d\n", storage4);
+	// 
+	// printf("storage5 : %d\n", storage5);
+	// 
+	// printf("storage6 : %d\n", storage6);
+#pragma endregion
+
+#pragma region if문
+	// 어떤 특정한 조건을 비교하여 조건이 맞다면
+	// 실행되는 조건문입니다.
+
+	//	int level = 99;
+	//	
+	//	if (level == 99)
+	//	{
+	//		printf("전직 가능\n");
+	//	}
+
+	// if문은 조건이 1일 때 실행되며, 0일 때 실행
+	// 되지 않습니다.
+#pragma endregion
+
+#pragma region else if문
+	// if문에서 여러 개의 조건을 순차적으로 검사하고
+	// 싶을 때 사용하는 조건문입니다.
+
+	//	int second = 3600;
+	//	
+	//	if (second == 60)
+	//	{
+	//		printf("1 minute");
+	//	}
+	//	else if (second == 3600)
+	//	{
+	//		printf("1 hour");
+	//	}
+
+	// else if문의 경우 여러 개를 사용할 수 있으며, if문
+	// 부터 위에서 하나씩 검사하다가 가장 먼저 조건이 맞는
+	// 분기로 들어갑니다.
+#pragma endregion
+
+
+#pragma endregion
 
 
 }
