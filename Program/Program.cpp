65de6cfa07d997767d0c92261a2e065feb57432d
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-07-21 15:44:52 +0900

    (2025.7.21) 스택

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -2,55 +2,8 @@
 
 using namespace std;
 
-class String
-{
-private:
-    int size;
-    char * pointer;
-
-public:
-    String()
-    {
-        size = 0;
-        pointer = nullptr;
-    }
-
-    void operator = (const char* word)
-    {
-        size = strlen(word) + 1;
-
-        if (pointer == nullptr)
-        {
-            pointer = new char[size];
-
-            for (int i = 0; i < size; i++)
-            {
-                pointer[i] = word[i];
-            }
-        }
-        else
-        {
-            char* container = new char[size];
-
-            for (int i = 0; i < size; i++)
-            {
-                container[i] = word[i];
-            }
-
-            delete [ ] pointer;
-
-            pointer = container;
-        }
-    }
-};
-
 int main()
 {
-    String message;
-
-    message = "League of Legend";
-
-    message = "Adidas";
-    
+  
     return 0;
 }
