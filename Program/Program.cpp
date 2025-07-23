f7b39498608318f4719e3777b1cf5ebc58501a6f
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-07-23 15:43:57 +0900

    (2025.7.23) 원형 큐

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,34 +1,118 @@
 ﻿#include <iostream>
 
-#define SIZE 4
-
 using namespace std;
 
+
 template <typename T>
-class Queue
+class PriorityQueue
 {
 private:
-    int rear;
-    int front;
+    int index;
+    int capacity;
 
-    T container[SIZE];
+    T * container;
+
 public:
-    Queue()
+    PriorityQueue()
     {
-        rear = SIZE - 1;
-        front = SIZE - 1;
+        index = 0;
+        capacity = 0;
 
-        for (int i = 0; i < SIZE; i++)
+        container = nullptr;
+    }
+
+    void resize(int newSize)
+    {
+        // 1. capacity에 새로운 size 값을 저정합니다.
+        capacity = newSize;
+
+        // 2. 새로운 포인터 변수를 생성해서 새롭게 만들어진
+        //    메모리 공간을 가리키도록 합니다.
+        T* pointer = new T[capacity];
+
+        // 3. 새로운 메모리 공간의 값을 초기화합니다.
+        for (int i = 0; i < capacity; i++)
         {
-            container[i] = NULL;
+            pointer[i] = NULL;
+        }
+
+        // 4. 기존 배열에 있는 값을 복사해서 새로운
+        //    배열에 넣어줍니다.
+        for (int i = 0; i < index; i++)
+        {
+            pointer[i] = container[i];
+        }
+
+        // 5. 기존 배열의 메모리를 해제합니다.
+        if (container != nullptr)
+        {
+            delete[] container;
+        }
+
+        // 6. 기존에 배열을 가리키던 포인터 변수의 값을
+        //    새로운 배열의 시작 주소로 가리킵니다.
+        container = pointer;
+    }
+
+    void push(T data)
+    {
+        if (capacity <= 0)
+        {
+            resize(1);
+        }
+        else if (index >= capacity)
+        {
+            resize(capacity * 2);
+        }
+
+        container[index++] = data;
+
+        int child = index - 1;
+        int parent = (child - 1) / 2;
+
+        while (child > 0)
+        {
+            if (container[parent] < container[child])
+            {
+                std::swap(container[parent], container[child]);
+            }
+
+            child = parent;
+
+            parent = (child - 1) / 2;
         }
     }
 
+    const T & top()
+    {
+        return container[0];
+    }
 
+    const bool & empty()
+    {
+        return index <= 0;
+    }
+
+    ~PriorityQueue()
+    {
+        if (container != nullptr)
+        {
+            delete[] container;
+        }
+    }
 };
 
 int main()
 {
+    PriorityQueue<int> priorityQueue;
+
+    priorityQueue.push(10);
+    priorityQueue.push(20);
+    priorityQueue.push(30);
+
+    cout << priorityQueue.top() << endl;
+    cout << priorityQueue.empty() << endl;
+
 
     return 0;
 }
