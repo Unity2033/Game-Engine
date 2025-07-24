6e77e014750e0d3d29aebdaf4d82504b5a625d99
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-07-24 15:43:45 +0900

    (2025.7.24) 우선 순위 큐

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,118 +1,92 @@
 ﻿#include <iostream>
 
+#define SIZE 6
+
 using namespace std;
 
-
-template <typename T>
-class PriorityQueue
+template<typename KEY, typename VALUE>
+class HashTable
 {
 private:
-    int index;
-    int capacity;
+    struct Node
+    {
+        KEY key;
+        VALUE value;
 
-    T * container;
+        Node * next;
+    };
 
+    struct Bucket
+    {
+        int count;
+        Node * head;
+    };
+
+    Bucket bucket[SIZE];
 public:
-    PriorityQueue()
+    HashTable()
     {
-        index = 0;
-        capacity = 0;
-
-        container = nullptr;
-    }
-
-    void resize(int newSize)
-    {
-        // 1. capacity에 새로운 size 값을 저정합니다.
-        capacity = newSize;
-
-        // 2. 새로운 포인터 변수를 생성해서 새롭게 만들어진
-        //    메모리 공간을 가리키도록 합니다.
-        T* pointer = new T[capacity];
-
-        // 3. 새로운 메모리 공간의 값을 초기화합니다.
-        for (int i = 0; i < capacity; i++)
+        for (int i = 0; i < SIZE; i++)
         {
-            pointer[i] = NULL;
-        }
-
-        // 4. 기존 배열에 있는 값을 복사해서 새로운
-        //    배열에 넣어줍니다.
-        for (int i = 0; i < index; i++)
-        {
-            pointer[i] = container[i];
-        }
-
-        // 5. 기존 배열의 메모리를 해제합니다.
-        if (container != nullptr)
-        {
-            delete[] container;
-        }
-
-        // 6. 기존에 배열을 가리키던 포인터 변수의 값을
-        //    새로운 배열의 시작 주소로 가리킵니다.
-        container = pointer;
-    }
-
-    void push(T data)
-    {
-        if (capacity <= 0)
-        {
-            resize(1);
-        }
-        else if (index >= capacity)
-        {
-            resize(capacity * 2);
-        }
-
-        container[index++] = data;
-
-        int child = index - 1;
-        int parent = (child - 1) / 2;
-
-        while (child > 0)
-        {
-            if (container[parent] < container[child])
-            {
-                std::swap(container[parent], container[child]);
-            }
-
-            child = parent;
-
-            parent = (child - 1) / 2;
+            bucket[i].count = 0;
+            bucket[i].head = nullptr;
         }
     }
 
-    const T & top()
+    Node* create_node(KEY key, VALUE value)
     {
-        return container[0];
+        Node * newNode = new Node;
+
+        newNode->key = key;
+        newNode->value = value;
+        newNode->next = nullptr;
+
+        return newNode;
     }
 
-    const bool & empty()
+    template <typename T>
+    const unsigned int & hash_function(T key)
     {
-        return index <= 0;
+        unsigned int hashIndex = (unsigned int)key % SIZE;
+
+        return hashIndex;
     }
 
-    ~PriorityQueue()
+    void insert(KEY key, VALUE value)
     {
-        if (container != nullptr)
+        // 해시 함수를 통해서 값을 받는 임시 변수
+        int hashIndex = hash_function(key);
+
+        // 새로운 노드를 생성합니다.
+        Node * newNode = create_node(key, value);
+
+        // 노드가 1개라도 존재하지 않는다면
+        if (bucket[hashIndex].count == 0)
         {
-            delete[] container;
+            // bucket[hashIndex]의 head 포인터가 newNode를 가리키게 합니다.
+            bucket[hashIndex].head = newNode;
         }
+        else
+        {
+            newNode->next = bucket[hashIndex].head;
+
+            bucket[hashIndex].head = newNode;
+        }
+
+        // bucket[hashIndex]의 count를 증가합니다.
+        bucket[hashIndex].count++;
     }
+
+
 };
 
 int main()
 {
-    PriorityQueue<int> priorityQueue;
+    HashTable<const char *, int> hashtable;
 
-    priorityQueue.push(10);
-    priorityQueue.push(20);
-    priorityQueue.push(30);
-
-    cout << priorityQueue.top() << endl;
-    cout << priorityQueue.empty() << endl;
-
+    hashtable.insert("Abyssal Mask ", 3000);
+    hashtable.insert("Bami’s Cinder ", 1000);
+    hashtable.insert("Chain Vest ", 800);
 
     return 0;
 }
