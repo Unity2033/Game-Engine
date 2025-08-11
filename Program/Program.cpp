073ae633060d2de0ce4ea6e52fe4ac146b408c2c
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-08-11 15:45:03 +0900

    (2025.8.11) 분할 정복

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -2,45 +2,45 @@
 
 using namespace std;
 
-void search(int list[], int key, int size)
+void merge_sort(int list[], int start, int end)
 {
-	int left = 0;
-	int right = size - 1;
-
-	while (left <= right)
+	if (start < end)
 	{
-		int pivot = (left + right) / 2;
+		int middle = (start + end) / 2;
 
-		if (list[pivot] == key)
-		{
-			cout << "key found : " << list[pivot] << endl;
-
-			return;
-		}
-		else if (list[pivot] > key)
-		{
-			right = pivot - 1;
-		}
-		else
-		{
-			left = pivot + 1;
-		}
+		merge_sort(list, start, middle);
+		merge_sort(list, middle + 1, end);
 	}
-
-	cout << "not key found" << endl;
 }
 
 int main()
 {
-#pragma region 이분 탐색
-	// 탐색 범위를 반으로 나누어 찾는 값을 포함하는 범위를
-	// 좁혀나가는 방식으로 동작하는 알고리즘입니다. 
+#pragma region 병합 정렬
+	// 하나의 리스트를 두 개의 균일한 크기로 분할하고 분할된
+	// 부분 리스트를 정렬한 다음, 두 개의 정렬된 부분 리스트를
+	// 합하여 전체가 정렬된 리스트가 되게 하는 방법입니다.
 
-	int list[] = { 5,6,11,13,27,55,66,92 };
+	// 1. 리스트의 길이가 0 또는 1이 되면 이미 정렬된 것으로 봅니다.
+
+	// 2. 그렇지 않은 경우
+
+	// 2-1. 정렬되지 않은 리스트를 절반으로 잘라 비슷한 크기의
+	//      두 부분 리스트로 나눕니다.
+
+	// 2-2. 각 부분 리스트를 재귀적으로 병합 정렬을 이용하여 정렬합니다.
+
+	// 2-3. 두 부분 리스트를 다시 하나의 정렬된 리스트로 병합합니다.
+
+	int list[] = { 3,5,2,7,4,1,8,6 };
 
 	int size = sizeof(list) / sizeof(list[0]);
 
+	merge_sort(list, 0, size - 1);
+
+
+
 #pragma endregion
+
 
 
 	return 0;
