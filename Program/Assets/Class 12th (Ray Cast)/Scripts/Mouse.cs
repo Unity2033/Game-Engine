0e3c4d184c4e30d9f22a55469b52b4c2673521cf
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-10-02 21:43:30 +0900

    (2025.10.2) 광선 검출 시스템

---
+++ Program/Assets/Class 12th (Ray Cast)/Scripts/Mouse.cs
@@ -0,0 +1,35 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Mouse : MonoBehaviour
+{
+    [SerializeField] Ray ray;
+    [SerializeField] float distance;
+    [SerializeField] Texture2D texture2D;
+    [SerializeField] RaycastHit rayCastHit;
+
+    private void Awake()
+    {
+        distance = Mathf.Infinity;
+
+        Cursor.SetCursor(texture2D, Vector2.zero, CursorMode.Auto);
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            Debug.DrawRay(ray.origin, ray.direction * distance, Color.green);
+
+            if(Physics.Raycast(ray, out rayCastHit, 100))
+            {
+                Debug.Log(rayCastHit.collider.name);
+
+                Debug.DrawLine(ray.origin, rayCastHit.point, Color.red);
+            }
+        }
+    }
+}
