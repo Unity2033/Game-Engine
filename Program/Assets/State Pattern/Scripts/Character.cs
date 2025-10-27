049592cb85167eab00a260f31515f3b303aa948e
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-10-27 21:43:47 +0900

    (2025.10.27) 명령 패턴

--- Program/Assets/State Pattern/Scripts/Character.cs
+++ Program/Assets/State Pattern/Scripts/Character.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Knife : Weapon
+public class Character : MonoBehaviour
 {
     [SerializeField] Animator animator;
 
@@ -9,7 +9,15 @@ public class Knife : Weapon
         animator = GetComponent<Animator>();
     }
 
-    public override void Attack()
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Attack();
+        }
+    }
+
+    public void Attack()
     {
         animator.SetTrigger("Attack");
     }
